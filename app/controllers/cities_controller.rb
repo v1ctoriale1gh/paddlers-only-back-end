f8a6efe411fa035ed6153c27899532007f4d6643
github username <github email address>
2020-08-05 15:42:35 -0400

    added cities index

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,2 +1,10 @@
 class CitiesController < ApplicationController
+    def index
+        #byebug
+        state = State.find(params[:state_id])
+        cities = state.cities
+        render json: cities, except: [:created_at, :updated_at]
+    end
+
+    
 end
