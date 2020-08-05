7de54702325dc923ca16e331d05b199541e448a4
github username <github email address>
2020-08-05 11:28:31 -0400

    added nested routes, states index route

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,2 +1,8 @@
 class StatesController < ApplicationController
+    
+    def index
+        states = State.all
+        render json: states, except: [:created_at, :updated_at]
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :events
-  resources :cities
-  resources :states
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :states, only: [:index] do
+    resources :cities, only: [:index, :new, :create] do
+      resources :events, only: [:index, :new, :create]
+      end
+    end  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
