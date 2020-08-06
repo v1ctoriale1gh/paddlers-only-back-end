1b30ea556bdd8964d1f4110da5ad97d7fdea7e22
github username <github email address>
2020-08-06 18:26:35 -0400

    got rid of bye bug

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,13 +11,14 @@ class EventsController < ApplicationController
         city = City.find(params[:city_id])
         #create the new event
         event = Event.create(event_params(:name, :contact, :address1, :address2, :city_id, :state, :zip, :date, :description))
-        byebug
-        #if the even is passes all validations
+        #byebug
+        #if the event passes all validations
         if event.valid?
           #order the events and stop showing eventss that are before today
           events = city.events.where('date >= ?', Date.today).order(:date)
           #send back the json
           render json: events, except: [:created_at, :updated_at]
+        # or send back an error message
         else
           render json: { errors: 'Failed to create create event. Please check that the date is after today and all fields are filled before submitting. Thank you!' }, status: :not_acceptable
         end
