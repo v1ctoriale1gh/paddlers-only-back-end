2cf843fd1253f535f8aa0e27f1ebcbffd636fbdc
github username <github email address>
2020-08-06 17:33:48 -0400

    added error if else to create route

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,11 +10,16 @@ class EventsController < ApplicationController
         #find the city from the params
         city = City.find(params[:city_id])
         #create the new event
-        Event.create(event_params(:name, :contact, :address1, :address2, :city_id, :state, :zip, :date, :description))
-        #order the events and stop showing eventss that are before today
-        events = city.events.where('date >= ?', Date.today).order(:date)
-        #send back the json
-        render json: events, except: [:created_at, :updated_at]
+        event = Event.create(event_params(:name, :contact, :address1, :address2, :city_id, :state, :zip, :date, :description))
+        #if the even is passes all validations
+        if event.valid?
+          #order the events and stop showing eventss that are before today
+          events = city.events.where('date >= ?', Date.today).order(:date)
+          #send back the json
+          render json: events, except: [:created_at, :updated_at]
+        else
+          render json: { errors: 'failed to create create event' }, status: :not_acceptable
+        end
     end
 
 private
