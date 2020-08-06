d457bf96b3ac09eb75b48a302f71263c108943d8
github username <github email address>
2020-08-06 17:26:04 -0400

    event create route

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,14 @@ class EventsController < ApplicationController
     end
 
     def create
-        
+        #find the city from the params
+        city = City.find(params[:city_id])
+        #create the new event
+        Event.create(event_params(:name, :contact, :address1, :address2, :city_id, :state, :zip, :date, :description))
+        #order the events and stop showing eventss that are before today
+        events = city.events.where('date >= ?', Date.today).order(:date)
+        #send back the json
+        render json: events, except: [:created_at, :updated_at]
     end
 
 private
